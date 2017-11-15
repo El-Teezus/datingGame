f7676ada12dc02c9ebfcda19c294f190872cb9a3
El-Teezus <lawrencethomp@gmail.com>
2017-11-15 17:21:49 -0500

    updated mainfile

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+"""
+main is where we have the initiated commands and probably the constructors. More on that later.
+"""
+
 import Controller
 import person
 import Item
 import Room
 import Player
 
-bardesc = ['you are now in the bar. bro there are some babes over there']
+gameDesc = "The Bad Thing is doing Things We Don't Like. We Need to Find a Way to Kill THE BAD THING. "
 
 
 
 def main():
     itsrunning = "true"
+    print(gameDesc)
     while itsrunning == "true":
-        print(bardesc)
         Controller.inputmessage = input('what are you going to do?')
         Controller.trymessage()
 
@@ -20,7 +24,6 @@ main()
 
 classroom = Room.Room('P132', 'COMP830: Object Oriented Software Development')
 untitled_not_really_mastered = Item.Item(47, 'dating game', 'probably the G.O.A.T', 'in our hearts')
-LT = Player.Player('here','LT')
 fellow_kids = person.Person('fellow kids', 'how do you do', 'the hallway', 'fellow kids')
 
 
