8a87595e60e0290342cf4cf4fec8eb580eeff65f
HannahYudkin <catgirlblue@gmail.com>
2017-12-05 15:55:00 -0500

    Update InventoryMain.py
    -Removed bugs

--- InventoryMain.py
+++ InventoryMain.py
@@ -20,11 +20,11 @@ def brunette():
     inventory = load_inventory()
     emma_gifts = load_emma_gifts()
     d = Dialog()
-    print('Choose from these options')
-    print ('Check Inventory')
-    print ('Check Location')
-    print ('Give gift to Emma')
-    print('Talk to Emma')
+    # print('Choose from these options')
+    # print ('Check Inventory')
+    # print ('Check Location')
+    print ('Give gift to Emma?')
+    print('Talk to Emma?')
     emma = Emma("likes", "dislikes", "neutral")
     selection = input('--->   ')
     while selection != "done":
@@ -32,6 +32,7 @@ def brunette():
             inventory.print_items()
         elif selection == "Check Location":
             print('Where you are (later)')
+
         elif selection == ('Give gift to Emma'):
             print('Which Item?')
             inventory.print_items()
@@ -40,49 +41,55 @@ def brunette():
             e.checkgift(gift)
             print('Current Relationship Level:')
             print(e.get_relationship())
+
+
+
         elif selection == 'Talk to Emma':
-            for i in range(3): ###Fix cause Colby is dumb###
+            for i in range(3):
                 if emma.relationship > 5:
                     if not d.is_empty('like'):
                         all = d.get_all("like")
                         s = random.choice(all)
                         print(s)
                         print(all)
+                        print("Current Relationship: ")
+                        print(emma.relationship)
                         if s == 'Do you you like puppies?':
                             print('1.) I do! They\'re amazing')
                             print('2.) No,I hate them.')
                             selection = input('--->   ')
                             if selection == "1":
+                                d.remove(s, "like")
                                 emma.relationship = emma.relationship + 4
-                                print('Me too! They\'re so fun to play with, and I really like their cute pup faces.(Hannah is lonely')
-                                d.remove(s, "like")
+                                print('\nMe too! They\'re so fun to play with, and I really like their cute pup faces.(Hannah is lonely')
                             elif selection == '2':
-                                emma.relationship = emma.relationship -4
-                                print('Oh...really? That\'s too bad, I think they\'re great...Well, anyway...')
                                 d.remove(s, "like")
+                                emma.relationship = emma.relationship -4
+                                print('\nOh...really? That\'s too bad, I think they\'re great...Well, anyway...')
                         elif s == 'I think you\'re really cool! Do you like me?':
                             print('1.) I think you\'re incredible.')
                             print('2.)I guess you just don\'t really do it for me.')
                             selection = input('--->   ')
                             if selection == "1":
+                                d.remove(s, "like")
                                 emma.relationship = emma.relationship + 4
-                                print('Emma blushes. "I\'m really happy that you think so".')
-                                d.remove(s, "like")
+                                print('\nEmma blushes. "I\'m really happy that you think so".')
                             elif selection == '2':
-                                print('Wow go fuck yourself.')
                                 d.remove(s, "like")
+                                emma.relationship = emma.relationship - 10
+                                print('Wow go fuck yourself.')
                         elif s == 'Do you want to go watch the sunset?':
                             print('1.)I actually am allergic to the sun, and your company. So no.')
                             print('2.) I think that sounds lovely right now. Should we leave now?')
                             selection = input('--->   ')
                             if selection == "1":
+                                d.remove(s, "like")
                                 emma.relationship = emma.relationship -5
-                                print('Ughh... I guess we shouldn\'t then...')
-                                d.remove(s, "like")
+                                print('\nUghh... I guess we shouldn\'t then...')
                             elif selection == '2':
-                                emma.relationship = emma.relationship + 6
-                                print('Yeah, lets go right now!!')
                                 d.remove(s, "like")
+                                emma.relationship = emma.relationship + 6
+                                print('\nYeah, lets go right now!!')
                         else:
                             print("Maybe?")
                         # d.remove(s, "like")
@@ -94,93 +101,90 @@ def brunette():
                         s = random.choice(all)
                         print(s)
                         print(all)
+                        print("Current Relationship: ")
+                        print(emma.relationship)
                         if s == 'Are you enjoying the town so far?':
                             print('1.) It\'s a mess and everyone here is poor. I hate it.')
                             print('2.) Everyone her seems very friendly! I\'ve been having a really great time here.')
                             selection = input('--->   ')
                             if selection == "1":
-                                print('How could you think something like that? Everyone here is fantastic, and you\'re just really judgemental.')
+                                d.remove(s, "neutral")
+                                print('\nHow could you think something like that? Everyone here is fantastic, and you\'re just really judgemental.')
                                 emma.relationship = emma.relationship - 6
-                                d.remove(s, "neutral")
                             elif selection == "2":
-                                print('I think so too! I\'ve always felt really at home here.')
-                                emma.relationship = emma.relationship + 4
                                 d.remove(s, "neutral")
+                                print('\nI think so too! I\'ve always felt really at home here.')
+                                emma.relationship = emma.relationship + 4
                         elif s == 'Were you here during the dragon attack?':
-                            print('I was. I helped a couple people into the sewers. We were all beat up, but we made it out okay.')
-                            print('Yeah I was. Does this place normally have such crazy stuff happening?')
+                            print('1.) I was. I helped a couple people into the sewers. We were all beat up, but we made it out okay.')
+                            print('2.) Yeah I was. Does this place normally have such crazy stuff happening?')
                             selection = input('--->   ')
                             if selection == "1":
-                                print('That was you? I\'m so glad you were there! Thank you so much!')
+                                d.remove(s, "neutral")
+                                print('\nThat was you? I\'m so glad you were there! Thank you so much!')
                                 emma.relationship = emma.relationship + 10
-                                d.remove(s, "neutral")
                             elif selection == "2":
-                                print('Not normally. It\s a really peaceful town normally.')
-                                emma.relationship = emma.relationship + 1
                                 d.remove(s, "neutral")
-                        # d.remove(s, "neutral")
+                                print('\nNot normally. It\s a really peaceful town normally.')
+                                emma.relationship = emma.relationship + 1
                         elif s == 'Are you planning on staying long?':
                             print('1.) Yeah, I think I\'ll stay a little bit longer.')
                             print('2.) I don\'t think I wanna stay in a place like this.')
                             selection = input('--->   ')
                             if selection == "1":
+                                d.remove(s, "neutral")
                                 emma.relationship = emma.relationship + 4
-                                print('I\'m glad you like it here!')
-                                d.remove(s, "like")
+                                print('\nI\'m glad you like it here!')
                             elif selection == '2':
+                                d.remove(s, "neutral")
                                 emma.relationship = emma.relationship - 4
-                                print('Well, if you feel like that it probably is a good idea to leave.')
-                                d.remove(s, "like")
+                                print('\nWell, if you feel like that it probably is a good idea to leave.')
                 if emma.relationship < -10:
-                    # if not d.is_empty('dislike'):
-                    #     all = d.get_all("dislike")
-                    #     s = random.choice(all)
-                    #     print(s)
-                    #     d.remove(s, "dislike")
                     if not d.is_empty('dislike'):
                         all = d.get_all("dislike")
                         s = random.choice(all)
                         print(s)
                         print(all)
+                        print("Current Relationship: ")
+                        print(emma.relationship)
                         if s == 'Umm... Are you here just to bother me?':
                             print('1.) I\'m sorry, I didn\'t mean to upset you. It\'s just been a stressful day.')
                             print('2.) Yeah I mean I don\'t have anything better to do.')
                             selection = input('--->   ')
                             if selection == "1":
+                                d.remove(s, "dislike")
                                 emma.relationship = emma.relationship + 5
-                                print('It\'s alright, I understand. Everyone here it feeling stressed out too.')
-                                d.remove(s, "dislike")
+                                print('\nIt\'s alright, I understand. Everyone here it feeling stressed out too.')
                             elif selection == '2':
-                                emma.relationship = emma.relationship -5
-                                print('Wow, alright. I actually have things better to do, so maybe you should just leave me alone.')
                                 d.remove(s, "dislike")
-                        elif s == 'You didn\'t help Arnold during the dragon attack?':
+                                emma.relationship = emma.relationship -5
+                                print('\nWow, alright. I actually have things better to do, so maybe you should just leave me alone.')
+                        elif s == 'You didn\'t help Max during the dragon attack?':
                             print('1.) Everyone needs to look out for themselves. I could have died trying to save them too.')
                             print('2.) No, I was a coward. I regret it deeply.')
                             selection = input('--->   ')
                             if selection == "1":
-                                emma.relationship = emma.relationship + 7
-                                print('"You don\'t really care about others, huh?"')
                                 d.remove(s, "dislike")
+                                emma.relationship = emma.relationship - 7
+                                print('\n"You don\'t really care about others, huh?"')
                             elif selection == '2':
-                                print('"I understand, I was really scared too. They both got out okay, so it all worked out."')
                                 d.remove(s, "dislike")
+                                emma.relationship = emma.relationship + 2
+                                print('\n"I understand, I was really scared too. They both got out okay, so it all worked out."')
                         elif s == 'Did you mean to annoy me?':
                             print('1.) Only a little bit. It\' kind of cute seeing you worked up.')
                             print('2.) I just don\'t care how you feel, that\'s all.')
                             selection = input('--->   ')
                             if selection == "1":
+                                d.remove(s, "dislike")
                                 emma.relationship = emma.relationship -5
-                                print('She laughs, but her face brightens up and she tries to hide her smile.')
-                                d.remove(s, "dislike")
+                                print('\nShe laughs, but her face brightens up and she tries to hide her smile.')
                             elif selection == '2':
-                                emma.relationship = emma.relationship + 6
-                                print('Yeah, lets go right now!!')
                                 d.remove(s, "dislike")
+                                emma.relationship = emma.relationship + 6
+                                print('\nYeah, lets go right now!!')
                         else:
                             print("Maybe?")
-                        # d.remove(s, "like")
-                        # emma.relationship = emma.relationship + 2
                         print(emma.relationship)
 
         else:
