389dda082105eb826a6c0c02c5628f2f2e45b010
TheWhiteHatter <TheWhiteHatter@users.noreply.github.com>
2017-12-11 17:32:26 -0500

    For iteration 5 Game Intro (blonde.py)

---
+++ blonde.py
@@ -0,0 +1,31 @@
+class BlondeC:
+
+        Questions = [["Blonde: Do you like my leather corset top?\nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde: Q2 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde: Q3 \nA) Yes\nB) Maybe\nC) No "],
+                ["Blonde Set 2: Q1 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 2: Q2 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 2: Q3 \nA) Yes\nB) Maybe\nC) No "],
+                ["Blonde Set 3: Q1 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 3: Q2 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 3: Q3 \nA) Yes\nB) Maybe\nC) No "],
+                ["Blonde  Set 4: Do you like my new dagger?\nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 4: Q2 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 4: Q3 \nA) Yes\nB) Maybe\nC) No "],
+                ["Blonde Set 5: Q1 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 5: Q2 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 5: Q3 \nA) Yes\nB) Maybe\nC) No "],
+                ["Blonde Set 6: Q1 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 6: Q2 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 6: Q3 \nA) Yes\nB) Maybe\nC) No "],                
+                ["Blonde Set 7: Q1 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 7: Q2 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 7: Q3 \nA) Yes\nB) Maybe\nC) No "],
+                ["Blonde Set 8: Q1 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 8: Q2 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 8: Q3 \nA) Yes\nB) Maybe\nC) No "],
+                ["Blonde Set 9: Q1 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 9: Q2 \nA) Yes\nB) Maybe\nC) No ",
+                    "Blonde Set 9: Q3 \nA) Yes\nB) Maybe\nC) No "]]
+
+
